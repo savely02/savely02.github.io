b00e5de03acb77d9077d7a932e55c32c1bcc16c3
savely02 <savely_s@mail.ru>
2020-12-01 21:44:03 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,12 +1,38 @@
 <?php
-
-$recepient = "savely_s@mail.ru";
-$sitename = "Название сайта";
-
-$name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
-
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
-
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name'])) {$name = $_POST['name'];}
+    if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+    // if (isset($_POST['email'])) {$email = $_POST['email'];}
+    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+ 
+    $to = "savely_s@mail.ru"; /*Укажите адрес, га который должно приходить письмо*/
+    $sendfrom   = "support@sitename.ru"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
+    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+    $subject = "$formData";
+    $message = "$formData
+ <b>Имя пославшего:</b> $name
+<b>Телефон:</b> $phone";
+    $send = mail ($to, $subject, $message, $headers);
+    if ($send == 'true')
+    {
+    echo '<center>
+ 
+Спасибо за отправку вашего сообщения!
+ 
+</center>';
+    }
+    else
+    {
+    echo '<center>
+ 
+<b>Ошибка. Сообщение не отправлено!</b>
+ 
+</center>';
+    }
+} else {
+    http_response_code(403);
+    echo "Попробуйте еще раз";
+}?>
